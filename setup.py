2e964ff7ce8ca8676c894e3e4052c53bae0a62c3
john <john@saponara.net>
2016-12-24 08:47:56 -0500

    combined rst format files into a single README file.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README', 'CHANGES')
+long_description = read('README')
 
 setup(
     name='opentaxforms',
