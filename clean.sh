34469aeac16d69e1acc6dd342f87cb56f40adc50
john <john@saponara.net>
2017-01-07 23:37:30 -0500

    - add casperjs script to test f1040 html files and shell script for context. - probe access to phantomjs,casperjs in travis, hopefully for future integration of html5 tests.

--- clean.sh
+++ clean.sh
@@ -2,6 +2,6 @@ find . -name '*.pyc' -exec rm {} \;
 find . -name '*.log' -exec rm {} \;
 find . -name __pycache__ -exec rm -rf {} \; >& /dev/null
 rm -rf script/{z,forms}
-rm -f test/forms/*[tlgc]
-rm -f {script,test}/failurls.*
+rm -f test/forms/*.{pickl,prof,txt,xml,html,svg} test/forms/static
+rm -f {script,test}/{*.log,*.prof,failurls.*}
 rm -f script/*.sqlite3
---
+++ test/run_html5.sh
@@ -0,0 +1,14 @@
+# test javascript in generated html files
+# todo generate a js test file for each form processed.
+# todo integrate with pytest and travis-ci.org.
+
+# one-time setup to run casperjs
+# sudo apt install npm phantomjs
+# init step avoids some warnings; see https://github.com/npm/npm/issues/9161
+# npm init --yes
+# npm install casperjs
+# node_modules/casperjs/bin/casperjs selftest
+# sudo ln -sf `pwd`/node_modules/casperjs/bin/casperjs /usr/local/bin/casperjs
+
+casperjs --local-storage-path=. test forms/f1040.js
+
