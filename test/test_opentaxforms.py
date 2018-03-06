5beacd098a01f0adf33ee41d29ed2b4ec564e641
john <john@saponara.us>
2018-03-06 08:37:02 -0500

    bugfixes in tests, especially in setup to ensure we start with eg non-empty database.

--- test/test_opentaxforms.py
+++ test/test_opentaxforms.py
@@ -2,7 +2,10 @@
 
 # 'Otf' below [eg TestOtfSteps]  is 'OpenTaxForms'
 
+from __future__ import print_function
 import os
+import os.path as osp
+import shutil
 from subprocess import check_output
 from opentaxforms import ut
 
@@ -22,12 +25,23 @@ def checkDependencies():
 
 class TestOtfBase(object):
     def setup_method(self, _):
+        dirName = 'forms'
+        theForm = 'f1040.pdf'
         dbpath='sqlite:///'+ut.Resource('test','opentaxforms.sqlite3').path()
-        os.remove('failurls.pickl')
+        if not osp.exists(dirName):
+            os.makedir(dirName)
+        formpath = osp.join(dirName, theForm)
+        if not osp.exists(formpath):
+            formResourcePath = ut.Resource('test', osp.join('forms-common', theForm)).path()
+            shutil.copy(formResourcePath, formpath)
+        filesToCleanup = 'failurls.pickl'.split()
+        for f in filesToCleanup:
+            if osp.exists(f):
+                os.remove(f)
         self.defaultArgs=dict(
             # skip cleanupFiles to allow comparison with target output
             #skip=['c'],
-            dirName='forms',
+            dirName = dirName,
             # todo change dbpath to dburl
             dbpath=dbpath,
             )
@@ -39,7 +53,7 @@ class TestOtfBase(object):
         rootForms=kw.get('rootForms') or '1040'
         filesToCheck=kw.get('filesToCheck') or ['f%s-p1.html'%(form,) for form in rootForms]
         kw.update(self.defaultArgs)
-        from opentaxforms import opentaxforms as otf
+        from opentaxforms import main as otf
         returnval=otf.opentaxforms(
             okToDownload=False,
             **kw)
@@ -47,15 +61,19 @@ class TestOtfBase(object):
             raise Exception('run failed, no output to compare against target')
         import filecmp
         shallow=False
-        outdir,targetdir='forms/','forms-targetOutput/'
+        outdir = 'forms'
+        targetdir= ut.Resource('test', 'forms-targetOutput').path()
         def fmtmsg(result,verb):
             return '{}: output file "{}" in "{}" {} target in "{}"'.format(
                 result,fileToCheck,outdir,verb,targetdir)
         for fileToCheck in filesToCheck:
-            filesMatch=filecmp.cmp(outdir+fileToCheck,targetdir+fileToCheck,shallow)
+            filesMatch=filecmp.cmp(
+                osp.join(outdir, fileToCheck),
+                osp.join(targetdir, fileToCheck),
+                shallow)
             if filesMatch:
                 result,verb='PASS','matches'
-                print fmtmsg(result,verb)
+                print(fmtmsg(result,verb))
             else:
                 result,verb='FAIL','does NOT match'
                 raise Exception(fmtmsg(result,verb))
@@ -87,7 +105,11 @@ class TestOtfSteps(TestOtfBase):
 class TestOtfApiBase(object):
     def setup_method(self, _):
         from opentaxforms.serve import createApp
-        dbpath='sqlite:///'+ut.Resource('test','opentaxforms.sqlite3').path()
+        dbResourcePath = ut.Resource('test',
+            osp.join('forms-common', 'f1040.sqlite3')).path()
+        dbFilePath = osp.join('test', 'opentaxforms.sqlite3')
+        shutil.copy(dbResourcePath, dbFilePath)
+        dbpath='sqlite:///' + dbFilePath
         self.app=createApp(dbpath=dbpath)
         self.client=self.app.test_client()
     def teardown_method(self, _):
@@ -96,18 +118,18 @@ class TestOtfApi(TestOtfApiBase):
     def test_api_orgn(self):
         # get list of organizations (currently just IRS)
         response=self.client.get('/api/v1/orgn')
-        assert '"code": "us_irs"' in response.data
-        assert response.status_code==200
+        assert response.status_code == 200
+        assert b'"code": "us_irs"' in response.data
     def test_api_f1040(self):
         # get form 1040
         response=self.client.get('/api/v1/form?q={"filters":[{"name":"code","op":"eq","val":"1040"}]}')
-        assert '"title": "Form 1040"' in response.data
-        assert response.status_code==200
+        assert response.status_code == 200
+        assert b'"title": "Form 1040"' in response.data
     def test_api_noresults(self):
         # request a nonexistent form
         response=self.client.get('/api/v1/form?q={"filters":[{"name":"code","op":"eq","val":"0000"}]}')
-        assert '"num_results": 0' in response.data
-        assert response.status_code==200
+        assert response.status_code == 200
+        assert b'"num_results": 0' in response.data
     def test_api_filterslots(self):
         # get all checkbox fields on page 1 of form 1040
         import json
@@ -120,11 +142,11 @@ class TestOtfApi(TestOtfApiBase):
         paramstring = json.dumps(dict(filters=filters))
         response = self.client.get(url%(paramstring,))
         assert response.status_code == 200
-        assert '"num_results": 15' in response.data
+        assert b'"num_results": 15' in response.data
 
 def main(args):
     def usage():
-        print 'usage: "%s -f" for fasttests or "%s -s" for slow tests'%(args[0],args[0])
+        print('usage: "%s -f" for fasttests or "%s -s" for slow tests'%(args[0],args[0]))
     if len(args)==2:
         if args[1]=='-f':
             testRunner=TestOtfApi()
