f8bc76fa0a6bed1c3232171670d2dcd66b916f3c
john <john@saponara.us>
2022-02-20 10:58:02 -0500

    output as executable format; accept filtering by year and filer; sort output.

--- opentaxforms/static/js/displayStorage.js
+++ opentaxforms/static/js/displayStorage.js
@@ -1,40 +1,48 @@
 
 var displayStorage = function(selector) {
-	// todo consider emitting code that would read the data into local storage
-	//   code = `var db=new window.Basil({namespace:'${namespace}',storage:'local'});` +
-	//          `db.set(form,{` ;
-	//   for(...)
-	//       code += `  ${key}:${vals[key]},`
-	//   code += `});`
-	//   tho js string interpolation via template literal: caniuse/06apr2018 says 92% tho not msie=2.5% [es6 shim for msie?  yes but this feature too syntax-y?]
-	var content = '',
-    code = '',
-		thisYear = new Date().getFullYear();
-	for (year=thisYear-10; year<thisYear; year++) {
-		var namespace='opentaxforms_'+year,
-			db=new window.Basil({namespace:namespace,storage:'local'}),
-			forms=db.keys();
-		if (forms.length) {
-			content+='namespace '+namespace+'\n';
-      code += `var db=new window.Basil({namespace:'${namespace}',storage:'local'});\n`;
-		}
-		for (var i=0; i<forms.length; i++) {
-			formname=forms[i];
-			content+='  form '+formname+'\n';
+  var 
+    //content = '',  // pretty outline, but useless.
+    code = '',       // executable format.
+    params = (new URL(window.location.href)).searchParams,
+    this_year = new Date().getFullYear(),
+    target_year = params.get("year"),
+    filer = params.get('filer'),
+    start_year = this_year - 10,
+    final_year = this_year ;
+  if (target_year) {
+    start_year = target_year;
+    final_year = target_year;
+  }
+  for (year=start_year; year<=final_year; year++) {
+    var namespace='opentaxforms_'+year,
+      db=new window.Basil({namespace:namespace,storage:'local'}),
+      forms=db.keys().sort();
+    if (forms.length) {
+      //content+='namespace '+namespace+'\n';
+    }
+    var form_codes = [];
+    for (var i=0; i<forms.length; i++) {
+      formname=forms[i];
+      if (filer && !formname.startsWith(filer)) continue;
+      //content+='  form '+formname+'\n';
       var code_form = '',
         quote = '';
-			vals=db.get(formname)||{};
-			for (var key in vals) {
-				if (vals[key]) {
-					content+='    '+key+' '+vals[key]+'\n';
-          quote = (typeof(vals[key]) === 'string' ? '"' : '')
+      vals=db.get(formname)||{};
+      for (var key in vals) {
+        if (vals[key]) {
+          //content+='    '+key+' '+vals[key]+'\n';
+          quote = (typeof(vals[key]) === 'string' && isNaN(Number(vals[key])) ? '"' : '')
           code_form += `  ${key}: ${quote}${vals[key]}${quote},\n`;
-				}
-			}
-      if (code_form) {
-        code += `db.set("${formname}", {\n${code_form}});\n`;
+        }
       }
-		}
-		document.querySelector(selector).value = code;
-	}
+      if (code_form) {
+        form_codes.push(`db.set("${formname}", {\n${code_form}});\n`);
+      }
+    }
+    if (form_codes.length) {
+      form_codes.unshift(`var db=new window.Basil({namespace:'${namespace}',storage:'local'});`);
+      code += form_codes.join('\n') + '\n';
+    }
+  }
+  document.querySelector(selector).value = code;
 }
